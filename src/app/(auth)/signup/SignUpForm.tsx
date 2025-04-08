7fdf82ba4fa595a15ac38ea68756aef41f096eba
chethazz <chethaskp10@gmail.com>
2025-04-08 16:14:01 +0530

    first commit

---
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Sign Up"
+};
+
+export default function SignUpForm() {
+    return (
+        <div className="flex h-full max-h-[32rem] w-full max-w-[32rem] rounded-2xl outline-hidden shadow-2xl bg-card">
+            Form here
+        </div>
+    );
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "./SignUpForm";
+
+export default function Page() {
+    return (
+        <main className="flex items-center justify-center h-screen p-5">
+                <SignUpForm />
+        </main>
+    );
+}
