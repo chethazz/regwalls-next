42f5b5aab10f21d6dab665a4d1f9541c74690b9c
chethazz <chethaskp10@gmail.com>
2025-04-08 17:56:27 +0530

    Finish sign up

--- src/app/(auth)/signup/SignUpForm.tsx
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -5,9 +5,13 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { signUp } from "./actions";
 
 export default function SignUpForm() {
+
+    const [error, setError] = useState<string>();
 
     const form = useForm<SignUpValues>({
         resolver: zodResolver(signUpSchema),
@@ -18,57 +22,70 @@ export default function SignUpForm() {
         }
     });
 
+    async function onSubmit(values: SignUpValues) {
+        const { error } = await signUp(values);
+        if (error) {
+            setError(error);
+        }
+    }
+
     return (
-            <Form {...form}>
-                <form className="w-full space-y-3">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Username" {...field} className="border-none bg-secondary" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full space-y-3"
+            >
+                {error && <p className="text-center text-destructive">{error}</p>}
 
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input type="email" placeholder="Email" {...field} className="border-none bg-secondary" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Username" {...field} className="border-none bg-secondary" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input type="password" placeholder="Password" {...field} className="border-none bg-secondary" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="Email" {...field} className="border-none bg-secondary" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-                    <Button
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Password" {...field} className="border-none bg-secondary" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button
                     className="w-full"
-                    >
-                        Create account
-                    </Button>
-                </form>
-            </Form>
+                    type="submit"
+                >
+                    Create account
+                </Button>
+            </form>
+        </Form>
     );
 }
---
+++ src/auth.ts
@@ -0,0 +1,38 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia } from "lucia";
+import prisma from "./lib/prisma";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        expires: false,
+        attributes: {
+            secure: process.env.NODE_ENV === "production"
+        }
+    },
+    getUserAttributes(databaseUserAttributes) {
+        return {
+            id: databaseUserAttributes.id,
+            username: databaseUserAttributes.username,
+            displayName: databaseUserAttributes.displayName,
+            avatarUrl: databaseUserAttributes.avatarUrl,
+            googleId: databaseUserAttributes.googleId
+        };
+    }
+});
+
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof lucia;
+        DatabaseUserAttributes: DatabaseUserAttributes;
+    }
+}
+
+interface DatabaseUserAttributes {
+    id: string;
+    username: string;
+    displayName: string;
+    avatarUrl: string | null;
+    googleId: string | null;
+}
