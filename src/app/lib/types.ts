3014524cd502e5ba3822cf9d300c06b4be8234bc
chethazz <chethaskp10@gmail.com>
2025-04-13 09:47:03 +0530

    types: userDataSelect: So that user is not null in WallpaperData

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,13 +1,15 @@
 import { Prisma } from "@prisma/client";
 
+export const userDataSelect = {
+    id: true,
+    username: true,
+    displayName: true,
+    avatarUrl: true
+} satisfies Prisma.UserSelect;
+
 export const wallpaperDataInclude = {
     user: {
-        select: {
-            id: true,
-            username: true,
-            displayName: true,
-            avatarUrl: true,
-        }
+        select: userDataSelect
     }
 } satisfies Prisma.ImageInclude;
 
