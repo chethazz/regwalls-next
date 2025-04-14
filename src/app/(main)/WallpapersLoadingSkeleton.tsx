7c045cb948393af9ec484c3380e13afb9790cf2b
chethazz <chethaskp10@gmail.com>
2025-04-14 07:28:05 +0530

    Better wallpaperloadingskeleton

--- src/app/(main)/WallpapersLoadingSkeleton.tsx
+++ src/app/(main)/WallpapersLoadingSkeleton.tsx
@@ -5,7 +5,13 @@ export default function WallpapersLoadingSkeleton() {
         <div className="w-full py-5 max-w-7xl">
             <div className="flex flex-col grid-cols-2 gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
                 {Array.from({ length: 8 }).map((_, i) => (
-                    <Skeleton key={i} className="h-80 bg-secondary" />
+                    <Skeleton key={i} className="flex items-end h-[22rem] bg-secondary">
+                        <Skeleton className="w-full p-4 space-y-3 h-fit">
+                            <Skeleton className="bg-gray-300 rounded-[5px] h-5 w-[40%]" />
+                            <Skeleton className="bg-gray-300 rounded-[5px] h-4 w-full" />
+                            <Skeleton className="bg-gray-300 rounded-[5px] h-4 w-full" />
+                        </Skeleton>
+                    </Skeleton>
                 ))}
             </div>
         </div>
