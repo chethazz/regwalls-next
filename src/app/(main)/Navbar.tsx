018663bab9ee22f0b817452220e71e8f0bc34fcf
chethazz <chethaskp10@gmail.com>
2025-04-15 16:14:35 +0530

    Navbar: Add create button

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,14 +1,23 @@
-import { UserRound } from "lucide-react";
+import { Plus, UserRound } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
     return (
         <header className="sticky top-0 z-10 shadow-sm bg-card">
-            <div className="flex flex-wrap items-center justify-center gap-5 px-5 py-3 mx-auto max-w-7xl">
+            <div className="flex flex-wrap items-center justify-between gap-5 px-5 py-3 mx-auto max-w-7xl">
                 <Link href="/" className="text-2xl font-bold text-primary">
                     Regwalls
                 </Link>
-                <UserRound className="sm:ms-auto" />
+                <div className="flex items-center justify-center gap-4">
+                    <Link
+                        href="/upload"
+                        className="flex items-center justify-center gap-2 px-3 py-2 bg-secondary rounded-2xl"
+                    >
+                        Create
+                        <Plus />
+                    </Link>
+                    <UserRound className="sm:ms-auto" />
+                </div>
             </div>
         </header>
     );
