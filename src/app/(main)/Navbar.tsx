2f29871c7d08d15a0ed66c658564a06180d2f971
chethazz <chethaskp10@gmail.com>
2025-04-15 16:19:28 +0530

    Better classes and changes to create button

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <header className="sticky top-0 z-10 shadow-sm bg-card">
-            <div className="flex flex-wrap items-center justify-between gap-5 px-5 py-3 mx-auto max-w-7xl">
+            <div className="flex flex-wrap items-center gap-5 px-5 py-3 mx-auto max-w-7xl">
                 <Link href="/" className="text-2xl font-bold text-primary">
                     Regwalls
                 </Link>
-                <div className="flex items-center justify-center gap-4">
+                <div className="flex items-center justify-center gap-4 sm:ms-auto">
                     <Link
                         href="/upload"
-                        className="flex items-center justify-center gap-2 px-3 py-2 bg-secondary rounded-2xl"
+                        className="flex items-center justify-center gap-2 px-3 py-2 shadow-2xl bg-primary text-secondary rounded-2xl"
                     >
                         Create
                         <Plus />
                     </Link>
-                    <UserRound className="sm:ms-auto" />
+                    <UserRound />
                 </div>
             </div>
         </header>
