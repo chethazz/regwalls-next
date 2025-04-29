c5841153943717809fd7dbfda4bc5b251c9adbcf
chethazz <chethaskp10@gmail.com>
2025-04-29 10:09:47 +0530

    Edit profile: Set up avatar input

--- src/app/(main)/users/[username]/EditProfileDialog.tsx
+++ src/app/(main)/users/[username]/EditProfileDialog.tsx
@@ -1,5 +1,6 @@
 import { UserData } from "@/app/lib/types";
 import { updateUserProfileSchema, UpdateUserProfileValues } from "@/app/lib/validation";
+import avatarPlaceholder from "@/assets/avatarPlaceholder.png";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
@@ -7,7 +8,12 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Camera } from "lucide-react";
+import Image, { StaticImageData } from "next/image";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import Resizer from "react-image-file-resizer";
+
 
 interface EditProfileDialogProps {
     user: UserData;
@@ -37,7 +43,9 @@ export default function EditProfileDialog({
                 </DialogHeader>
                 <div className="space-y-2">
                     <Label>Profile picture</Label>
-                    Avatarinput
+                    <AvatarInput
+                        src={user.avatarUrl || avatarPlaceholder}
+                    />
                 </div>
 
                 <Form {...updateUserProfileForm}>
@@ -89,5 +97,60 @@ export default function EditProfileDialog({
                 </Form>
             </DialogContent>
         </Dialog>
+    );
+}
+
+interface AvatarInputProps {
+    src: string | StaticImageData;
+}
+
+function AvatarInput({
+    src
+}: AvatarInputProps) {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [selectedImage, setSelectedImage] = useState<string | StaticImageData>(src);
+
+
+    function onImageSelected(image: File | undefined) {
+        if (!image) return;
+
+        Resizer.imageFileResizer(
+            image,
+            1024,
+            1024,
+            "WEBP",
+            100,
+            0,
+            (uri) => { if (typeof uri === "string") setSelectedImage(uri); },
+            "base64"
+        );
+    }
+
+    return (
+        <>
+            <Input
+                type="file"
+                accept="image/*"
+                onChange={(e) => onImageSelected(e.target.files?.[0])}
+                ref={fileInputRef}
+                className="hidden sr-only"
+            />
+            <button
+                type="button"
+                onClick={() => fileInputRef.current?.click()}
+                className="relative block group"
+            >
+                <Image
+                    src={selectedImage}
+                    alt="Profile picture"
+                    width={150}
+                    height={150}
+                    className="flex-none object-cover rounded-full size-32"
+                />
+                <span className="absolute bottom-0 right-0 flex items-center justify-center text-white transition-colors duration-200 bg-black rounded-full size-10 bg-opacity-30 group-hover:bg-opacity-20 ">
+                    <Camera size={24} />
+                </span>
+            </button>
+        </>
     );
 }
