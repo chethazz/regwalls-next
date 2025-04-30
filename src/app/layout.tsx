847a224c561a2c49d66b5586b723bb090740fd71
chethazz <chethaskp10@gmail.com>
2025-04-30 15:34:43 +0530

    Add theme support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import ReactQueryProvider from "./(main)/ReactQueryProvider";
@@ -28,12 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            {children}
+          </ThemeProvider>
           <Toaster richColors position="bottom-right" />
         </ReactQueryProvider>
       </body>
