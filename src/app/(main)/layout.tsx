cb4e9c18ee42d5cc7b38096d7ec75230a98c90fc
chethazz <chethaskp10@gmail.com>
2025-04-30 11:04:20 +0530

    SessionProvider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,19 @@
+import { validateRequest } from "@/auth";
 import Navbar from "./Navbar";
+import SessionProvider from "./SessionProvider";
 
 export default async function Layout({
     children
 }: { children: React.ReactNode; }) {
+
+    const session = await validateRequest();
+
     return (
-        <div className="flex flex-col min-h-screen">
-            <Navbar />
-            {children}
-        </div>
+        <SessionProvider value={session}>
+            <div className="flex flex-col min-h-screen">
+                <Navbar />
+                {children}
+            </div>
+        </SessionProvider>
     );
 }
