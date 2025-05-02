be03ae88bcd27085d14b2e2b355facf6c39fe2b6
chethazz <chethaskp10@gmail.com>
2025-05-02 10:11:31 +0530

    types: UserWallpaperData

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -12,6 +12,31 @@ export type UserData = Prisma.UserGetPayload<{
     select: typeof userDataSelect;
 }>;
 
+export function getWallpaperDataInclude(loggedInUserId: string) {
+    return {
+        user: {
+            select: userDataSelect
+        },
+        image: {
+            select: {
+                imageUrl: true
+            }
+        },
+        favorites: {
+            where: {
+                userId: loggedInUserId
+            },
+            select: {
+                userId: true
+            }
+        }
+    } satisfies Prisma.WallpaperInclude;
+}
+
+export type LoggedInWallpaperData = Prisma.WallpaperGetPayload<{
+    include: ReturnType<typeof getWallpaperDataInclude>;
+}>;
+
 export const wallpaperDataInclude = {
     user: {
         select: userDataSelect
@@ -20,7 +45,7 @@ export const wallpaperDataInclude = {
         select: {
             imageUrl: true
         }
-    }
+    },
 } satisfies Prisma.WallpaperInclude;
 
 export type WallpaperData = Prisma.WallpaperGetPayload<{
@@ -29,4 +54,8 @@ export type WallpaperData = Prisma.WallpaperGetPayload<{
 
 export interface WallpapersPage {
     wallpapers: WallpaperData[];
+}
+
+export interface FavoriteInfo {
+    isFavoritedByUser: boolean;
 }
