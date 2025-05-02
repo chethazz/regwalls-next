8d368ac69abd3775b306ff57fb09f41f24481ddd
chethazz <chethaskp10@gmail.com>
2025-05-02 10:32:46 +0530

    wallpapers: route: Separate return if logged in

--- src/app/api/wallpapers/route.ts
+++ src/app/api/wallpapers/route.ts
@@ -1,8 +1,23 @@
-import { wallpaperDataInclude, WallpapersPage } from "@/app/lib/types";
+import { getUserWallpaperDataInclude, wallpaperDataInclude, WallpapersPage } from "@/app/lib/types";
+import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
     try {
+        const { user } = await validateRequest();
+
+        if (user) {
+            const wallpapers = await prisma.wallpaper.findMany({
+                include: getUserWallpaperDataInclude(user.id)
+            });
+
+            const data: WallpapersPage = {
+                wallpapers
+            };
+
+            return Response.json(data);
+        }
+
         const wallpapers = await prisma.wallpaper.findMany({
             include: wallpaperDataInclude
         });
