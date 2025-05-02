b109b5e1b2fe7dc93a37fcb0aa55dfd2807d7fc4
chethazz <chethaskp10@gmail.com>
2025-05-02 11:03:07 +0530

    FavoriteButton: Show log in or sign up when not logged in and minor visual tweaks

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useSession } from "@/app/(main)/SessionProvider";
 import { FavoriteInfo } from "@/app/lib/types";
 import kyInstance from "@/lib/ky";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { Heart } from "lucide-react";
+import LoginOrCreate from "./LoginOrCreate";
+import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 interface FavoriteButtonProps {
     wallpaperId: string;
@@ -18,12 +21,28 @@ export default function FavoriteButton({
     className
 }: FavoriteButtonProps) {
 
+    const { user } = useSession();
+
     const { data } = useQuery({
         queryKey: ["favorite-info", wallpaperId],
         queryFn: () => kyInstance.get(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`).json<FavoriteInfo>(),
         initialData: initialState,
-        staleTime: Infinity
+        staleTime: Infinity,
+        enabled: !!user
     });
+
+    if (!user) {
+        return (
+            <div className={className}>
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="cursor-pointer">
+                        <Heart className="transition-all hover:scale-110 hover:animate-pulse hover:fill-red-400 hover:text-red-400" />
+                    </DropdownMenuTrigger>
+                    <LoginOrCreate />
+                </DropdownMenu>
+            </div>
+        );
+    }
 
     function onFavoriteClick() {
         if (data.isFavoritedByUser) {
@@ -44,8 +63,8 @@ export default function FavoriteButton({
                 onFavoriteClick();
             }}
         >
-            <Heart className={cn(
-                data.isFavoritedByUser && "fill-red-600 text-red-600 hover:fill-red-400 hover:text-red-400"
+            <Heart className={cn("transition-all hover:scale-110 hover:animate-pulse hover:fill-red-400 hover:text-red-400",
+                data.isFavoritedByUser && "fill-red-600 text-red-600"
             )}
             />
         </button>
