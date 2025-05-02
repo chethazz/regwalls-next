a5c4947c7bb292644e96c10abe062f8f89db4412
chethazz <chethaskp10@gmail.com>
2025-05-02 10:07:16 +0530

    favorite GET end point

--- src/app/api/wallpapers/[wallpaperId]/favorite/route.ts
+++ src/app/api/wallpapers/[wallpaperId]/favorite/route.ts
@@ -1,5 +1,38 @@
+import { FavoriteInfo } from "@/app/lib/types";
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+
+export async function GET(
+    { params }: { params: Promise<{ wallpaperId: string; }>; }
+) {
+    try {
+        const { wallpaperId } = await params;
+
+        const { user: loggedInUser } = await validateRequest();
+
+        if (!loggedInUser) {
+            return Response.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const favorite = await prisma.favorite.findUnique({
+            where: {
+                userId_wallpaperId: {
+                    userId: loggedInUser.id,
+                    wallpaperId
+                }
+            }
+        });
+
+        const data: FavoriteInfo = {
+            isFavoritedByUser: !!favorite
+        };
+
+        return Response.json(data);
+    } catch (error) {
+        console.error(error);
+        return Response.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
 
 export async function POST(
     req: Request,
