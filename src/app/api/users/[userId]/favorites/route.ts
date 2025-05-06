a9b8afeb305757c9ad46205fc2c94d285f5b8f59
chethazz <chethaskp10@gmail.com>
2025-05-06 10:47:55 +0530

    favorites: GET endpoint

---
+++ src/app/api/users/[userId]/favorites/route.ts
@@ -0,0 +1,32 @@
+import { wallpaperDataInclude, WallpapersPage } from "@/app/lib/types";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+    req: Request,
+    { params }: { params: Promise<{ userId: string; }>; }
+) {
+    try {
+        const { userId } = await params;
+
+        if (!userId) throw new Error("User Id is required");
+
+        const favorites = await prisma.favorite.findMany({
+            where: { userId },
+            include: {
+                wallpaper: {
+                    include: wallpaperDataInclude
+                }
+            },
+            orderBy: { createdAt: "desc" },
+        });
+
+        const data: WallpapersPage = {
+            wallpapers: favorites.map(fav => fav.wallpaper)
+        };
+
+        return Response.json(data);
+    } catch (error) {
+        console.error(error);
+        return Response.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
