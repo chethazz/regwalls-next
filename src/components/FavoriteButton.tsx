899c019e63d5400d0948ea725561f9a9888a3f17
chethazz <chethaskp10@gmail.com>
2025-05-07 09:39:59 +0530

    FavoriteButton: Mutation

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -4,10 +4,11 @@ import { useSession } from "@/app/(main)/SessionProvider";
 import { FavoriteInfo } from "@/app/lib/types";
 import kyInstance from "@/lib/ky";
 import { cn } from "@/lib/utils";
-import { useQuery } from "@tanstack/react-query";
+import { QueryKey, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Heart } from "lucide-react";
 import LoginOrCreate from "./LoginOrCreate";
 import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { toast } from "sonner";
 
 interface FavoriteButtonProps {
     wallpaperId: string;
@@ -27,12 +28,41 @@ export default function FavoriteButton({
 
     const { user } = useSession();
 
+    const queryClient = useQueryClient();
+
+    const queryKey: QueryKey = ["bookmark-info", wallpaperId];
+
     const { data } = useQuery({
-        queryKey: ["favorite-info", wallpaperId],
+        queryKey: queryKey,
         queryFn: () => kyInstance.get(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`).json<FavoriteInfo>(),
         initialData: initialState,
         staleTime: Infinity,
         enabled: !!user
+    });
+
+    const { mutate } = useMutation({
+        mutationFn: () =>
+            data.isFavoritedByUser
+                ? kyInstance.delete(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`)
+                : kyInstance.post(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`),
+        onMutate: async () => {
+            toast.message(`${data.isFavoritedByUser ? "Removed from favorites" : "Added to favorites"}`);
+
+            await queryClient.cancelQueries({ queryKey });
+
+            const previousState = queryClient.getQueryData<FavoriteInfo>(queryKey);
+
+            queryClient.setQueryData<FavoriteInfo>(queryKey, () => ({
+                isFavoritedByUser: !previousState?.isFavoritedByUser
+            }));
+
+            return { previousState };
+        },
+        onError(error, variables, context) {
+            queryClient.setQueryData(queryKey, () => context?.previousState);
+            console.error(error);
+            toast.error("Something went wrong. Please try again.");
+        },
     });
 
     if (!user) {
@@ -52,14 +82,6 @@ export default function FavoriteButton({
         );
     }
 
-    function onFavoriteClick() {
-        if (data.isFavoritedByUser) {
-            kyInstance.delete(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`);
-        } else {
-            kyInstance.post(`http://localhost:3000/api/wallpapers/${wallpaperId}/favorite`);
-        }
-    }
-
     return (
         <button className={cn(
             "cursor-pointer flex justify-center items-center gap-2 group",
@@ -68,7 +90,7 @@ export default function FavoriteButton({
             onClick={(e) => {
                 e.stopPropagation();
                 e.preventDefault();
-                onFavoriteClick();
+                mutate();
             }}
         >
             <Heart className={cn("transition-all hover:scale-110 hover:animate-pulse hover:fill-red-400 hover:text-red-400 group-hover:scale-110 group-hover:animate-pulse group-hover:fill-red-400 group-hover:text-red-400",
