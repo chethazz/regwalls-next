18e6b3b70db1ea8b2c572ccfb09316cdf3b2e359
chethazz <chethaskp10@gmail.com>
2025-05-10 15:04:02 +0530

    Fix navbar

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 
     return (
         <header className="sticky top-0 z-10 shadow-sm bg-card">
-            <div className="flex flex-wrap items-center gap-5 px-5 py-3 mx-auto max-w-7xl">
+            <div className="flex flex-wrap items-center justify-between gap-5 px-5 py-3 mx-auto max-w-7xl">
                 <Link href="/" className="text-2xl font-bold text-primary">
                     Regwalls
                 </Link>
