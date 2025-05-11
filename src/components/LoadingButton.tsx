948793b1e324ad9e9ae81f38ac7101901a14e442
chethazz <chethaskp10@gmail.com>
2025-05-11 10:22:31 +0530

    components: LoadingButton

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { Button, ButtonProps } from "./ui/button";
+
+interface LoadingButtonProps extends ButtonProps {
+    loading: boolean;
+}
+
+export default function LoadingButton({
+    children,
+    loading,
+    disabled,
+    className,
+    ...props
+}: LoadingButtonProps) {
+    return (
+        <Button
+            disabled={loading || disabled}
+            className={cn("flex items-center gap-2", className)}
+            {...props}
+        >
+            {children}
+        </Button>
+    );
+}
